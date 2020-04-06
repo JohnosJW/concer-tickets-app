5953a785838690eb77b1019e62185db42f8e65cb
Aynur <johnosjw@gmail.com>
2020-04-06 08:02:14 +0300

    Subtitle may be like null

--- tests/Feature/AddConcertTest.php
+++ tests/Feature/AddConcertTest.php
@@ -147,4 +147,50 @@ class AddConcertTest extends TestCase
         $response->assertSessionHasErrors('title');
         $this->assertEquals(0, Concert::count());
     }
+
+    /** @test */
+    public function testSubTitleIsOptional()
+    {
+        $this->withoutExceptionHandling();
+
+        /** @var  $user */
+        $user = factory(User::class)->create();
+
+        /** @var  $response */
+        $response = $this->actingAs($user)->post('/backstage/concerts', [
+            'title' => 'No Warning',
+            'subtitle' => '',
+            'additional_information' => "You must be 19 years of age to attend this concert.",
+            'date' => '2017-11-18',
+            'time' => '8:00pm',
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Fake St.',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '12345',
+            'ticket_price' => '32.50',
+            'ticket_quantity' => '75',
+        ]);
+
+        tap(Concert::first(), function ($concert) use ($response, $user) {
+            $response->assertRedirect('/backstage/concerts');
+
+            $this->assertTrue($concert->user->is($user));
+
+            $this->assertFalse($concert->isPublished());
+
+            $this->assertEquals('No Warning', $concert->title);
+            $this->assertNull($concert->subtitle);
+            $this->assertEquals("You must be 19 years of age to attend this concert.", $concert->additional_information);
+            $this->assertEquals(Carbon::parse('2017-11-18 8:00pm'), $concert->date);
+            $this->assertEquals('The Mosh Pit', $concert->venue);
+            $this->assertEquals('123 Fake St.', $concert->venue_address);
+            $this->assertEquals('Laraville', $concert->city);
+            $this->assertEquals('ON', $concert->state);
+            $this->assertEquals('12345', $concert->zip);
+            $this->assertEquals(3250, $concert->ticket_price);
+            $this->assertEquals(75, $concert->ticket_quantity);
+            $this->assertEquals(0, $concert->ticketsRemaining());
+        });
+    }
 }
